ab1676a9050e54298e77e70ece374991eff53393
chenyushuo <297086016@qq.com>
2018-04-08 21:49:25 +0800

    ljn's homework

--- The third Homework/2017201979刘炯楠/find_url.h
+++ The third Homework/2017201979刘炯楠/find_url.h
@@ -1,21 +1,23 @@
 #ifndef FIND_URL_H
 #define FIND_URL_H
 
-
+#include <iostream>
 #include <string>
 
-const int URL_SIGN_LENGTH = 8;
-/*
-#ifndef MAXLEN_
-#define MAXLEN_
-const int MAXLEN = 200;
-#endif
-*/
+class Download_file{
+	private :
+		const static int URL_SIGN_LENGTH = 8;
+		std::string m_page_name;
+		int file_len;
+		std::string file_content;
+		FILE * open_page(const std::string page_name);
+	public :
+		Download_file(const std::string page_name);	
+		~Download_file() {}
+		
+		int count_url() const;
+		void get_url(std::string* & url_list, const std::string & limit_name) const;
+};
+
 const std::string URL_SIGN = "a href=\"";
-
-
-inline static FILE* open_page(const std::string web_page);
-int count_url(const std::string now_web_page);
-void get_url(std::string* & url_list, const std::string & now_web_page, const std::string & limit_name);
-
 #endif
