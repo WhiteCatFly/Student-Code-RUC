687181c2415bafba5928d58ac77fae7684a6d31e
yyqchemic <yyqchemic@hotmail.com>
2018-07-07 21:53:56 +0800

    8th hw

---
+++ The eighth Homework/杨越千-2017201975/programs/check/1.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string x;
+	while(1)
+	{
+		getline(cin, x);
+		cout << x << endl;
+	}
+	return 0;
+}
---
+++ The eighth Homework/杨越千-2017201975/programs/check/2.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	ofstream f("22");
+	f << 1;
+	
+	return 0;
+}
